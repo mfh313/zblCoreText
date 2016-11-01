1f7fb173168088658721f384b6c8e40664fbec1f
mafanghua <980992720@qq.com>
2016-11-01 10:39:47 +0800

    Initial Commit

---
+++ CoreTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextDemo
+//
+//  Created by EEKA on 2016/11/1.
+//  Copyright © 2016年 EEKA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreTextDemo
+//
+//  Created by EEKA on 2016/11/1.
+//  Copyright © 2016年 EEKA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
